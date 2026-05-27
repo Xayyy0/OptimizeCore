8a4cdde3a99ebab9cda5ef7fbdc573671f2ad79e
Xayyy0 <168991561+Xayyy0@users.noreply.github.com>
2026-05-27 18:09:34 -0400

    Add files via upload

---
+++ src/cosmicpe/form/entries/simple/Button.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace cosmicpe\form\entries\simple;
+
+use cosmicpe\form\entries\FormEntry;
+use cosmicpe\form\types\Icon;
+
+final class Button implements FormEntry{
+
+	private string $title;
+
+	private ?Icon $icon;
+
+	public function __construct(string $title, ?Icon $icon = null){
+		$this->title = $title;
+		$this->icon = $icon;
+	}
+
+	public function jsonSerialize() : array{
+		return [
+			"text" => $this->title,
+			"image" => $this->icon
+		];
+	}
+}
